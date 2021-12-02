fe0d970de38c475fc3fee44521587ed194ddc898
Charles Martin <charles.martin@gmail.com>
2021-12-02 10:40:11 -0800

    Move test to separate directory, rename position

--- a02.hs
+++ a02.hs
@@ -1,17 +1,15 @@
 -- https://adventofcode.com/2021/day/2
 
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module A02 where
 
 import Control.Lens
-import Data.String.QQ
 
-data Sub = Sub { _subH, _subD, _subA :: !Int } deriving Show
+data Sub = Sub { _subP, _subD, _subA :: !Int } deriving Show
 makeLenses ''Sub
 zSub = Sub 0 0 0
-answer Sub{..} = _subH * _subD
+answer Sub{..} = _subP * _subD
 
 main = do
     s <- getContents <&> lines
@@ -19,31 +17,20 @@ main = do
     print $ solve cmd2 s
 
 solve cmd s =
-    flip (,) <*> answer $
-    foldl' (&) zSub $ map (parse cmd) s
+    answer $ foldl' (&) zSub $ map (parse cmd) s
 
 parse cmd s = case words s of
     [k,x] -> cmd k (read x)
     _ -> error s
 
 cmd1 = \case
-    "forward" -> over subH . (+)
+    "forward" -> over subP . (+)
     "down"    -> over subD . (+)
     "up"      -> over subD . subtract
     e -> error e
 
 cmd2 = \case
-    "forward" -> \x s@Sub{..} -> s & over subH (+x) . over subD (+ (_subA * x))
+    "forward" -> \x s@Sub{..} -> s & over subP (+x) . over subD (+ (_subA * x))
     "down"    -> over subA . (+)
     "up"      -> over subA . subtract
     e -> error e
-
-test :: String
-test = [s|
-forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2
-|]
