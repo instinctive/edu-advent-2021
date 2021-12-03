c7a34e40c8334b585e1eeacfdcdf5f78d4efbb6b
Charles Martin <charles.martin@gmail.com>
2021-12-03 11:46:15 -0800

    C++ solution for Day 3

---
+++ c03.cpp
@@ -0,0 +1,76 @@
+// https://adventofcode.com/2021/day/3
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    std::istream_iterator<std::string> start(cin), end;
+    std::vector<std::string> ary(start,end);
+    const int ndata = ary.size();
+    const int nbits = ary[0].size();
+
+    // ----------------------------------------------------------------------
+    // Part One
+
+    int gamma = 0;
+    for (int i=0; i<nbits; ++i) {
+        int nz = 0;
+        for (auto& s : ary) {
+            if (s[i] == '0') ++nz;
+        }
+        gamma *= 2;
+        if (nz * 2 <= ndata) gamma += 1;
+    }
+    const int epsilon = (1 << nbits) - gamma - 1;
+    cout << "Part 1: " << gamma * epsilon << " = " << gamma << " * " << epsilon << endl;
+
+    // ----------------------------------------------------------------------
+    // Part Two
+    
+    std::sort(ary.begin(),ary.end());
+
+    // Count the number of zeros at this index. Requires sorted order.
+    auto count = [&ary](int index, int lo, int hi) {
+        while (true) {
+            int md = (lo + hi) / 2;
+            if (md == lo) return hi;
+            if (ary[md][index] == '0') {
+                lo = md;
+            } else {
+                hi = md;
+            }
+        }
+    };
+
+    // Calculate rating for this criteria.
+    auto rating = [&ary,ndata,nbits,&count](bool isCO2) {
+        int value = 0, lo = 0, hi = ndata;
+        for (int i=0; i<nbits; ++i) {
+            value *= 2;
+            // When there is a single number left, use it.
+            if (lo+1 == hi) {
+                if (ary[lo][i] == '1') value += 1;
+                continue;
+            }
+            // Otherwise count zeros and apply bit criteria.
+            int nz = count(i,lo,hi);
+            if (((nz - lo) * 2 <= hi - lo) == isCO2) {
+                hi = nz;
+            } else {
+                lo = nz;
+                value += 1;
+            }
+        }
+        return value;
+    };
+    const int oxygen = rating(false);
+    const int carbon = rating(true);
+    cout << "Part 2: " << oxygen * carbon << " = " << oxygen << " * " << carbon << endl;
+}
