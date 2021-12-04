34d12e8d0bf7272baa7ff70c6c55c70e2e2e788f
Charles Martin <charles.martin@gmail.com>
2021-12-03 16:15:14 -0800

    Better output

--- a01.hs
+++ a01.hs
@@ -5,7 +5,10 @@ module A01 where
 main = do
     xx <- getContents <&> map read . lines
     let x3 = [ a+b+c | (a:b:c:_) <- tails xx ]
-    print $ count xx
-    print $ count x3
+    answer 1 $ count xx
+    answer 2 $ count x3
 
 count s = length . filter id $ zipWith (<) s (tail s)
+
+answer :: Show a => Int -> a -> IO ()
+answer i a = printf "Part %d: " i >> print a
