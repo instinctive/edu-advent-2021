1ef1a01184a2e83e63b3a2bd3ec7e41360374747
Charles Martin <charles.martin@gmail.com>
2021-12-12 13:38:22 -0800

    Refactor out of main

--- h11.hs
+++ h11.hs
@@ -6,23 +6,26 @@ import qualified Data.Map.Strict as M
 
 main = do
     start <- parse <$> getContents
-    let z = M.map (const 0) start
-    let reset m = M.union m z
-    let seq = iterate (step . reset) start
-    printf "Part 1: %d\n" $ part1 100 (M.size start) seq
-    printf "Part 2: %d\n" $ part2 seq
+    let (one,two) = solve start
+    printf "Part 1: %d\n" one
+    printf "Part 2: %d\n" two
   where
-    step = fromLeft undefined . iterateM flash . M.map succ
 
 parse raw = M.fromList
     [ (V2 r c, digitToInt i)
     | (r,s) <- zip [0..] $ lines raw
     , (c,i) <- zip [0..] s ]
 
-part1 n z mm = n * z - calc mm where
-    calc = sum . take n . tail . map M.size
-
-part2 = fromJust . findIndex ((==0).M.size)
+solve start =
+    (100 * n - one seq, two seq)
+  where
+    one = sum . take n . tail . map M.size
+    two = fromJust . findIndex ((==0).M.size)
+    seq = iterate (step . reset) start
+    step = fromLeft undefined . iterateM flash . M.map succ
+    reset m = M.union m z
+    z = M.map (const 0) start
+    n = M.size start
 
 flash prev
     | M.null over = Left under
@@ -34,8 +37,3 @@ flash prev
 
 orbit = let dd = V2 <$> [-1..1] <*> [-1..1] in
     \v -> (+v) <$> dd
-
-idempotent f xx =
-    getAlt . mconcat . zipWith go xx $ tail xx
-  where
-    go a b = Alt $ if f a == f b then Just a else Nothing
