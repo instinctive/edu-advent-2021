cbd312e6d37745b1e7e7e36644a3e9962193611a
Charles Martin <charles.martin@gmail.com>
2021-12-14 07:45:17 -0800

    Refactor in prep for memoization

--- h14.hs
+++ h14.hs
@@ -1,7 +1,10 @@
 module H14 where
 
 import Control.Monad.State.Strict (execState,modify)
+import qualified Data.Map.Monoidal.Strict as MM
 import qualified Data.Map.Strict as M
+
+type Map = MM.MonoidalMap
 
 main = do
     temp <- getLine
@@ -12,14 +15,20 @@ main = do
 
 parse = f . words where f [[a,b],_,[c]] = ((a,b),c)
 
-solve rules temp n =
-    maximum counts - minimum counts
+single :: Char -> Map Char (Sum Int)
+single c = MM.singleton c 1
+
+solve rules temp n = getSum $
+    maximum xx - minimum xx
   where
-    counts = M.elems . flip execState M.empty . go $ (,n) <$> temp
-    add c = modify $ M.insertWith (+) c 1
-    go [(a,_)]            = add a >>          pure ()
-    go ((a,0):more)       = add a >>          go more
-    go ((a,_):(b,0):more) = add a >> add b >> go more
-    go ((a,i):(b,j):more) = go ((a,i):(c,k):(b,j):more) where
-        c = rules M.! (a,b)
-        k = min i j - 1
+    start = (,n) <$> temp
+    inner = mconcat $ zipWith (counts rules) start (tail start)
+    outer = mconcat $ single <$> temp
+    xx = MM.elems $ inner <> outer
+
+counts rules = go where
+    go ai@(a,i) bj@(b,j)
+        | i == 0 || j == 0 = MM.empty
+        | otherwise = go ai ck <> go ck bj <> single c
+      where
+        ck@(c,_) = (rules M.! (a,b), min i j - 1)
